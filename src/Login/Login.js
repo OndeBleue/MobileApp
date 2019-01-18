1cd03d72fa7205d34cf4b455fe00c57c9cca34a1
Sylvain <sylvain@sbx.pm>
2019-01-18 11:04:37 +0100

    style login form

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -5,9 +5,11 @@ class Login extends Component {
   render() {
     return(
       <div className="login">
-        <h1> Connexion </h1>
-		<form>
-			<input type="text" />
+        <img src="logo.png" alt="logo" />
+		<form className="login-form">
+			<label htmlFor="name">Je me connecte pour la première fois</label>
+			<input id="name" placeholder="Mon nom" type="text" />
+			<button>OK</button>
 		</form>
       </div>
     );
