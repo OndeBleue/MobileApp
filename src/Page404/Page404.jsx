43f4df6fa199f87bfddca1129732b6201de3b99e
Sylvain <bond.never.die@gmail.com>
2019-03-04 20:07:35 +0100

    handle page not found

--- src/Page404/Page404.jsx
+++ src/Page404/Page404.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from "react";
-import "./Page404.css";
+import React, { Component } from 'react';
+import './Page404.css';
+
+import logo from './logo.png';
 
 class Page404 extends Component {
+  handleRedirect = () => {
+    this.props.history.push('/login');
+  };
+
   render() {
     return(
       <div className="page404">
         <h1> Page non trouvée </h1>
+        <p>Essayez de vous propager ailleurs ...</p>
+        <img src={logo} alt="logo" onClick={this.handleRedirect}/>
       </div>
     );
   }
